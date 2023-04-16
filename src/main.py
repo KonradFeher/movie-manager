732ce61aa348980ddbb9d641f3ae248b4d80a57e
KonradFeher <konradfeher@outlook.com>
2023-04-16 17:25:00 +0200

    Modularity, Pathlib, <Return> binding, blank MainPage added

--- src/main.py
+++ src/main.py
@@ -1,168 +1,31 @@
-from abc import ABC, abstractmethod
 from tkinter import *
 import customtkinter
-import urllib3
-import requests
-from urllib3.exceptions import HTTPError
-from PIL import Image
 from models.User import User
-
-configs = {}
-API_KEY = '352322dd49f426559fac64881f6ecbb9'
-
-
-class SearchParams(object):
-    def __init__(self):
-        self.title = ''
-
-
-# TODO: export pages into seperate file(s?)
-class Page(ABC):
-    @staticmethod
-    @abstractmethod
-    def get_page_title():
-        pass
-
-    @staticmethod
-    @abstractmethod
-    def get_page_size():
-        pass
-
-
-class LoginPage(customtkinter.CTkFrame, Page):
-
-    @staticmethod
-    def get_page_title():
-        return "MovieManager Login"
-
-    @staticmethod
-    def get_page_size():
-        return "600x580"
-
-    def __init__(self, master: customtkinter.CTkFrame, controller, **kwargs):
-        super().__init__(master, **kwargs)
-
-        self.logo = customtkinter.CTkImage(
-            # TODO: paths should not be windows hardcoded, use pathlib
-            light_image=Image.open("src\\assets\\icon_1.png"),
-            dark_image=Image.open("src\\assets\\icon_2.png"),
-            size=(200, 200)
-        )
-
-        self.lbl_logo = customtkinter.CTkLabel(master=self, image=self.logo, text="", corner_radius=25)
-        self.lbl_logo.pack(anchor="center", pady=(50, 10), padx=30)
-
-        self.lbl_username = customtkinter.CTkLabel(master=self, text="Username")
-        self.lbl_username.pack(anchor="center", pady=(25, 0), padx=30)
-        self.username = StringVar()
-        self.ent_username = customtkinter.CTkEntry(master=self, width=400, corner_radius=5, placeholder_text="Username")
-        self.ent_username.pack(anchor="center", pady=10, padx=30)
-
-        self.lbl_password = customtkinter.CTkLabel(master=self, text="Password")
-        self.lbl_password.pack(anchor="center", pady=0, padx=30)
-        self.password = StringVar()
-        self.ent_password = customtkinter.CTkEntry(master=self, width=400, corner_radius=5, placeholder_text="Password", show="•")
-        self.ent_password.pack(anchor="center", pady=10, padx=30)
-
-        self.frm_buttons = customtkinter.CTkFrame(master=self, fg_color="transparent")
-        self.frm_buttons.grid_rowconfigure(0, weight=1)  # configure grid system
-        self.frm_buttons.grid_columnconfigure(0, weight=1)
-        self.frm_buttons.grid_columnconfigure(1, weight=1)
-        self.frm_buttons.login_button = customtkinter.CTkButton(
-            master=self.frm_buttons,
-            width=200,
-            height=40,
-            corner_radius=20,
-            text="Log In",
-            command=controller.login_user
-        )
-        self.frm_buttons.login_button.grid(row=0, column=0, pady=5, padx=10)
-        self.frm_buttons.register_button = customtkinter.CTkButton(
-            master=self.frm_buttons,
-            width=200,
-            height=40,
-            corner_radius=20,
-            text="Register",
-            command=lambda: controller.show_frame(RegisterPage)
-        )
-        self.frm_buttons.register_button.grid(row=0, column=1, pady=5, padx=10)
-        self.frm_buttons.pack(anchor="center", pady=(25, 50), padx=20)
-
-
-class RegisterPage(customtkinter.CTkFrame, Page):
-
-    @staticmethod
-    def get_page_title():
-        return "MovieManager Register"
-
-    @staticmethod
-    def get_page_size():
-        return "600x740"
-
-    def __init__(self, master: customtkinter.CTkFrame, controller, **kwargs):
-        super().__init__(master, **kwargs)
-
-        self.logo = customtkinter.CTkImage(
-            light_image=Image.open("src\\assets\\icon_1.png"),
-            dark_image=Image.open("src\\assets\\icon_2.png"),
-            size=(200, 200)
-        )
-
-        self.lbl_logo = customtkinter.CTkLabel(master=self, image=self.logo, text="", corner_radius=25)
-        self.lbl_logo.pack(anchor="center", pady=(50, 10), padx=30)
-
-        self.lbl_username = customtkinter.CTkLabel(master=self, text="Username")
-        self.lbl_username.pack(anchor="center", pady=(25, 0), padx=30)
-        self.username = StringVar()
-        self.ent_username = customtkinter.CTkEntry(master=self, width=400, corner_radius=5, placeholder_text="the_Dude", textvariable=self.username)
-        self.ent_username.pack(anchor="center", pady=10, padx=30)
-
-        self.lbl_email = customtkinter.CTkLabel(master=self, text="E-mail Address")
-        self.lbl_email.pack(anchor="center", pady=0, padx=30)
-        self.email = StringVar()
-        self.ent_email = customtkinter.CTkEntry(master=self, width=400, corner_radius=5, placeholder_text="big_lebowski@gmail.com", textvariable=self.email)
-        self.ent_email.pack(anchor="center", pady=10, padx=30)
-
-        self.lbl_password = customtkinter.CTkLabel(master=self, text="Password")
-        self.lbl_password.pack(anchor="center", pady=0, padx=30)
-        self.password = StringVar()
-        self.ent_password = customtkinter.CTkEntry(master=self, width=400, corner_radius=5, placeholder_text="Password", show="•", textvariable=self.password)
-        self.ent_password.pack(anchor="center", pady=10, padx=30)
-
-        self.lbl_password_again = customtkinter.CTkLabel(master=self, text="Password Confirmation")
-        self.lbl_password_again.pack(anchor="center", pady=0, padx=30)
-        self.password_again = StringVar()
-        self.ent_password_again = customtkinter.CTkEntry(master=self, width=400, corner_radius=5, placeholder_text="Password Confirmation", show="•", textvariable=self.password_again)
-        self.ent_password_again.pack(anchor="center", pady=10, padx=30)
-
-        self.register_button = customtkinter.CTkButton(
-            master=self,
-            width=200,
-            height=40,
-            corner_radius=20,
-            text="Register",
-            command=lambda: controller.register_user(User(self.username, self.email, self.password, self.password_again))
-        )
-        self.register_button.pack(anchor="center", pady=(25, 50), padx=20)
-
-        self.btn_back = customtkinter.CTkButton(
-            master=self,
-            height=30,
-            corner_radius=15,
-            text='Back',
-            command=lambda: controller.show_frame(LoginPage),
-            fg_color="#142d5e",
-            text_color="#5584CC"
-        )
-        self.btn_back.place(anchor="nw", x=20, y=20)
+from src.API import APIaccess
+from src.pages.MainPage import MainPage
+from src.pages.LoginPage import LoginPage
+from src.pages.RegisterPage import RegisterPage
 
 
 # https://www.digitalocean.com/community/tutorials/tkinter-working-with-classes
 class App(customtkinter.CTk):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.current_page = None
+        self.api = APIaccess('352322dd49f426559fac64881f6ecbb9')
+
+        ###############################################################
+        #                    API TEST ZONE                            #
+        ###############################################################
+
+        # print(self.api.fetch_movies("The Big Lebowski"))
+        # print(self.api.fetch_movies("Cars"))
+        # print(self.api.fetch_movies("Finding Nemo"))
+
+        ###############################################################
+
         customtkinter.set_appearance_mode("System")
-        customtkinter.set_default_color_theme("dark-blue")           # #2fa572
+        customtkinter.set_default_color_theme("dark-blue")  # #2fa572
         self.minsize(width=520, height=600)
         self.title("MovieManager")
         self.geometry("600x580")
@@ -175,32 +38,29 @@ class App(customtkinter.CTk):
         self.container.grid_columnconfigure(0, weight=1)
 
         self.frames = dict()
-        for F in (LoginPage, RegisterPage):
+        for F in (LoginPage, RegisterPage, MainPage):
             frame = F(self.container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(LoginPage)
+        self.bind("<Return>", lambda e: self.handle_return())
+        self.show_page("LoginPage")
         # self.center_window()
 
-    def show_frame(self, cont):
-        frame = self.frames[cont]
+    def show_page(self, cont):
+        self.current_page = cont;
+        page = eval(cont)
+        frame = self.frames[page]
         frame.tkraise()
-        self.title(cont.get_page_title())
-        print(cont.get_page_size())
-        self.geometry(cont.get_page_size())
-        # self.center_window()
+        self.title(page.get_page_title())
+        # print(page.get_page_size())
+        self.geometry(page.get_page_size())
+        self.center_window()
 
-    def login_user(self):
-        pass
-
-    def register_user(self, user):
-        pass
-
-# https://stackoverflow.com/questions/14910858/how-to-specify-where-a-tkinter-window-opens
-    def center_window(self, width, height):
-        w = width
-        h = height
+    # https://stackoverflow.com/questions/14910858/how-to-specify-where-a-tkinter-window-opens
+    def center_window(self, width=1, height=1):
+        w = self._current_width
+        h = self._current_height
 
         # get screen width and height
         ws = self.winfo_screenwidth()  # width of the screen
@@ -214,50 +74,41 @@ class App(customtkinter.CTk):
         # and where it is placed
         self.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
+    def login_user(self):
+        l_p = self.frames[LoginPage]
+        print("User login attempt:", l_p.ent_username.get())
 
-def get_configs():
-    global configs
-    try:
-        print('Fetching API configs.')
-        configs = requests.get("https://api.themoviedb.org/3/configuration?", params={'api_key': API_KEY}).json()
-    except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')
-    except Exception as err:
-        print(f'Other error occurred: {err}')
-    else:
-        print('Success!')
+        if l_p.ent_username.get() == 'letmein':
+            print("legit.")
+            self.show_page("MainPage")
+
+        # TODO
+
+    def register_user(self):
+        r_p = self.frames[RegisterPage]
+        print("User registering:")
+        print(r_p.ent_username.get())
+        print(r_p.ent_email.get())
+        pw_match = r_p.ent_password.get() != r_p.ent_password_again.get()
+        print("Passwords", "" if pw_match else "don't", "match.")
+        # TODO
+
+    def handle_return(self):
+        if self.current_page == "RegisterPage":
+            self.register_user()
+        elif self.current_page == "LoginPage":
+            self.register_user()
+        elif self.current_page == "MainPage":
+            print("🤡")
+            # todo?
 
 
 if __name__ in {"__main__", "__mp_main__"}:
-    get_configs()
     app = App()
 
-    # https://stackoverflow.com/a/9109106
+    # https://stackoverflow.com/a/9109106 - starting minimized fix
     app.attributes('-topmost', 1)
     app.update()
     app.attributes('-topmost', 0)
 
     app.mainloop()
-
-
-
-
-# self.sp = SearchParams()
-#
-#         # self.grid_rowconfigure("all", weight=1)  # configure grid system
-#         # self.grid_columnconfigure("all", weight=1)
-#
-#         self.search_title_label = customtkinter.CTkLabel(master=self, text="Movie title")
-#         # self.search_title_label.grid(row=0, column=0, sticky="w", pady=10, padx=10)
-#         self.search_title_label.pack(anchor="center", expand=1)
-#
-#         self.search_title = StringVar()
-#         self.search_title_entry = customtkinter.CTkEntry(master=self, width=400, corner_radius=5, placeholder_text="The Big Lebowski")
-#         # self.search_title_entry.grid(row=1, column=0, sticky="we")
-#         self.search_title_entry.pack(anchor="center")
-#
-#         self.search_button = customtkinter.CTkButton(master=self, width=200, corner_radius=10, text="Search", command=self.search)
-#         # self.search_button.grid(row=1, column=1)
-#         self.search_button.pack(anchor="center", expand=1)
-#
-#         self.withdraw()
